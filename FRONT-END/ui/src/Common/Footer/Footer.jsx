4f6f38779fc7d82c160cad9da30342e86b0f9e28
Hari4775 <harikumar.v@armbyte.com>
2024-06-16 23:50:27 +0530

    hari-header-footer-updated

---
+++ FRONT-END/ui/src/Common/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-red-600 w-screen '>
+        <h1  className='text-center text-green-500 text-5xl font-bold'> from H2</h1>
+      
+    </div>
+  )
+}
+
+export default Footer
--- FRONT-END/ui/src/User/UserIndex.jsx
+++ FRONT-END/ui/src/User/UserIndex.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './Post-login/Pages/HomePage/HomePage'
+import Header from '../Common/Header/Header'
+import Footer from '../Common/Footer/Footer'
 const UserIndex = () => {
   return (
     <>
-
+    <Header/>
      <Routes>
       <Route path="/" element={<HomePage/>} />   
     </Routes>
+    <Footer/>
       
     </>
   )
