eb9b1e91acb299c82b974ed1c1beef8a6a39bcc3
Harikumar <harikumarv9000@gmail.com>
2024-07-08 20:50:05 +0530

    updated latest

---
+++ FRONT-END/ui/src/User/Components/Virtual/Virtual.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Virtual.css'
+
+const Virtual = () => {
+  return (
+    <div className='virtual w-screen h-screen'>
+      <h1>sldfhsdfkjhlsdfhkl</h1>
+    </div>
+  )
+}
+
+export default Virtual
