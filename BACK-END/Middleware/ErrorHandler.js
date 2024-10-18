4f8c24f2d6a381ff42c29a403875b156d33f3c32
harikumar V <harikumarv9000@gmail.com>
2024-10-18 18:18:38 +0530

    new backend

--- BACK-END/Middleware/ErrorHandler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-};
-
-module.exports = errorHandler;
--- BACK-END/Middleware/upload.js
+++
@@ -1,6 +0,0 @@
-const multer = require("multer");
-
-const storage = multer.diskStorage({})
-const upload =multer({storage:storage})
-
-module.exports = upload
